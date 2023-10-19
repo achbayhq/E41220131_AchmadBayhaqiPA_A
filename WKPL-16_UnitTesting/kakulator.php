21e0bd53b6b32a89dc611eadaa92e7edf69742fa
achbayhq <achmadbayhaqiprog@gmail.com>
2023-10-19 13:38:34 +0700

    tugas WKPL acara 16

--- WKPL-16_UnitTesting/kakulator.php
+++ WKPL-16_UnitTesting/kakulator.php
@@ -1,7 +1,7 @@
 <?php 
 class kakulator{  //saya membuat class kakulator
-    public function tambah($num1, $num2){ //membuat function tambah dengan 2 constructor yang nantinya akan ditambahkan
-        return $num1 + $num2;               //return hasil dari kedua constructor yang telah ditambahkan
+    public function tambah($num1, $num2){ //membuat function tambah dengan 2 parameter yang nantinya akan ditambahkan
+        return $num1 + $num2;               //return hasil dari kedua parameter yang telah ditambahkan
     }
 }
 ?>
